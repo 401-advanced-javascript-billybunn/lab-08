2e53186f5900e79ac5022459b88c96a138bb90a2
Billy Bunn <williammbunn@gmail.com>
2019-03-27 16:21:04 -0700

    starts validate method on model

--- models/categories.js
+++ models/categories.js
@@ -2,6 +2,14 @@
 
 const uuid = require('uuid/v4');
 
+// ??? required: false
+// ??? type: string, array, object, number
+const schema = {
+  id: { required: true },
+  name: { required: true },
+  display_name: {require: true},
+};
+
 class Categories {
 
   constructor() {
@@ -10,17 +18,34 @@ class Categories {
 
   get(_id) {
   }
-  
+
   post(record) {
     record.id = uuid();
-    this.database.push(record);
-    return record;
+    if (this.validate(record)) {
+      this.database.push(record);
+      return record;
+    } else {
+      return null;
+    }
   }
 
   put(_id, record) {
   }
 
   delete(_id) {
+  }
+
+  validate(record) {
+    // check properties of the schema against the record
+    let valid = true;
+
+    Object.keys(schema).forEach(field => {
+      if (schema[field].required) {
+        if (!record[field]) { valid = false; }
+      }
+    });
+
+    return valid;
   }
 
 }
